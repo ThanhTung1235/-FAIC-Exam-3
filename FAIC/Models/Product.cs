eab5dd1f6deae4a4018eb281295b7c5b5a74f6f5
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-11-24 10:17:39 +0700

    First commit

---
+++ FAIC/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAIC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int  ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string  SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public string UnitslnStock { get; set; }
+        public string  UnitsOnOrder { get; set; }
+        public string ReorderLevel { get; set; }
+        public string Discontinued { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
